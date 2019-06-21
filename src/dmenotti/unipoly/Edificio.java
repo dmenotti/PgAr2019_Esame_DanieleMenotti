dad29c22f55a7135b86d593a49fa1faf07f358e7
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-06-21 17:00:11 +0200

    doc fine

--- src/dmenotti/unipoly/Edificio.java
+++ src/dmenotti/unipoly/Edificio.java
@@ -4,9 +4,9 @@ public class Edificio extends Casella {
 	public static final int TIPO_EDIF_NESSUNO = 0;
 	public static final int TIPO_EDIF_CASA = 1;
 	public static final int TIPO_EDIF_ALBERGO = 2;
-	public static final int BASE_COSTO = 5000;
+	public static final int BASE_COSTO = 5000;			//Base di costo e guadagno sotto la quale non si scende
 	public static final int BASE_GUADAGNO = 500;
-	public static final double MOLT_BLOCCO = 1.3;
+	public static final double MOLT_BLOCCO = 1.3;		//In caso il giocatore avesse tutto il blocco di case/alberghi viene applicato un moltiplicatore
 	
 	private int costoCasa;
 	private int costoAlbergo;
@@ -19,7 +19,7 @@ public class Edificio extends Casella {
 
 	public Edificio(int id, int tipo, String nome, int settore) {
 		super(id, tipo, nome);
-		costoCasa = (int) (BASE_COSTO * (1.0 + ((double)id/50.0)));
+		costoCasa = (int) (BASE_COSTO * (1.0 + ((double)id/50.0)));					//Determino il costo e guadagno dei vari edifici, basandomi sulla loro posizione nel tabellone (più è alta più gli edifici sono costosi)
 		costoAlbergo = (int) (BASE_COSTO * (1.5 + (((double)id*2.0)/50.0)));
 		guadCasa = (int) (BASE_GUADAGNO * (1.0 + ((double)id/50.0)));
 		guadAlbergo = (int) (BASE_GUADAGNO * (1.5 + (((double)id*2.0)/50.0)));
--- src/dmenotti/unipoly/IncorrectSizeException.java
+++ src/dmenotti/unipoly/IncorrectSizeException.java
@@ -1,7 +1,7 @@
 package dmenotti.unipoly;
 
-public class IncorrectSizeException extends Exception {
-	private static final long serialVersionUID = -4764903282040312176L;
+public class IncorrectSizeException extends Exception { //Verso la fine mi sono reso conto che avrei poteto non crearla, ma ormai ci sarebbe stato da cambiare tutto il codice...
+	private static final long serialVersionUID = -4764903282040312176L;			//Un'eccezione molto base invocata nel caso si inserisse un numero di caselle o stazioni troppo basso
 
 
 	public IncorrectSizeException(String message) {
